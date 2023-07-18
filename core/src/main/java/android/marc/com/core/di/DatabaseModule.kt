7a296c8a168e7f99d1052f3fedaa96ec3ebe8266
Mario Claudius <mcmario7901@gmail.com>
2023-07-18 19:48:13 +0700

    implement database encryption using SQLCipher

--- core/src/main/java/android/marc/com/core/di/DatabaseModule.kt
+++ core/src/main/java/android/marc/com/core/di/DatabaseModule.kt
@@ -6,17 +6,26 @@ import android.marc.com.core.data.source.local.room.CharacterDatabase
 import androidx.room.Room
 import dagger.Module
 import dagger.Provides
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
 class DatabaseModule {
 
+    @Provides
+    fun provideHelperFactory() : SupportFactory = SupportFactory(
+        SQLiteDatabase.getBytes("harrypotter".toCharArray())
+    )
+
     @Singleton
     @Provides
-    fun provideDatabase(context: Context): CharacterDatabase = Room.databaseBuilder(
+    fun provideDatabase(context: Context, factory: SupportFactory): CharacterDatabase = Room.databaseBuilder(
         context,
         CharacterDatabase::class.java, "Character.db"
-    ).fallbackToDestructiveMigration().build()
+    ).fallbackToDestructiveMigration()
+        .openHelperFactory(factory)
+        .build()
 
     @Provides
     fun provideCharacterDao(database: CharacterDatabase) : CharacterDao = database.characterDao()
