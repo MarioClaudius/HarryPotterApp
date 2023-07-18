e1c061437a1f907c1bce704a4901560e745301c1
Mario Claudius <mcmario7901@gmail.com>
2023-07-18 20:45:57 +0700

    implement certificate pinning

--- core/src/main/java/android/marc/com/core/di/NetworkModule.kt
+++ core/src/main/java/android/marc/com/core/di/NetworkModule.kt
@@ -3,6 +3,7 @@ package android.marc.com.core.di
 import android.marc.com.core.data.source.remote.api.ApiService
 import dagger.Module
 import dagger.Provides
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -13,11 +14,18 @@ import java.util.concurrent.TimeUnit
 class NetworkModule {
 
     @Provides
-    fun provideOkHttpClient(): OkHttpClient {
+    fun provideCertificatePinner(): CertificatePinner = CertificatePinner.Builder()
+        .add("hp-api.onrender.com", "sha256/FhZ/ntpC0jSQ+ZBqPeQVbLCf29Pb+k9Ki31luISkWQo=")
+        .add("hp-api.onrender.com", "sha256/8xoDcrs2LNLr7MXkvzFEmk5ILiKRIILkK1sEKWZy5Oo=")
+        .build()
+
+    @Provides
+    fun provideOkHttpClient(certificatePinner: CertificatePinner): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
